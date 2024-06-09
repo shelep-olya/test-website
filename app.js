8078b8fe646a30122923495632e101354d1b1caa
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-06-09 18:29:55 +0300

    improving security

--- app.js
+++ app.js
@@ -1,81 +1,44 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
-const { postResFunc } = require("./utils/test-functionallity");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const userRouter = require("./routes/userRoutes");
 const testRouter = require("./routes/testRoutes");
 const viewsRouter = require("./routes/viewsRoutes");
-//ІНДЕКС ВІДПОВІДАЄ ЗА ШАБЛОН ЯКИЙ Я ТАК І НЕ ЗРОБИЛА. ЯКЩО ЗАХОЧЕШ ПОВЕРНУТИ НАЗАД
-//ІНДЕКС УДАЛИ І ЗРОБИ МЕЙН ЧЕРЕЗ ПОВНОЦІННИЙ HTML + CHSNGE APP.GET
+const AppError = require("./utils/app-error");
+
 const app = express();
-
-// Nалаштування шаблонів EJS
+app.use(helmet());
 app.set("view engine", "ejs");
-
-// Парсери для даних
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Статичні файли
 app.use(express.static("./public"));
-
-// Ліміт запитів
-const limiter = rateLimit({
-  max: 1000,
-  windowMs: 60 * 60 * 1000, // 1 година
-  message: "Too many requests. Please try again in an hour.",
-});
-app.use("/", limiter);
-
-// Маршрути
 app.use("/auth", userRouter);
 app.use("/test", testRouter);
 app.use("/", viewsRouter);
 
-// Головна сторінка
-// app.get("/" || "/main", (req, res) => {
-//   const locals = "home";
-
-//   res.render("main", { locals });
-// });
-
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests. Please try again in an hour.",
+});
+app.use("/", limiter);
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitize());
+app.use(xss());
+//ІНДЕКС ВІДПОВІДАЄ ЗА ШАБЛОН ЯКИЙ Я ТАК І НЕ ЗРОБИЛА. ЯКЩО ЗАХОЧЕШ ПОВЕРНУТИ НАЗАД
+//ІНДЕКС УДАЛИ І ЗРОБИ МЕЙН ЧЕРЕЗ ПОВНОЦІННИЙ HTML + CHSNGE APP.GET
 // app.get("/me.ejs", (req, res) => {
 //   res.render("me");
 // });
 // app.get("/moreTests.ejs", (req, res) => {
 //   res.render("moreTests");
 // });
-// app.get("/about.ejs", (req, res) => {
-//   res.render("about");
-// });
-// app.get("/results.ejs", (req, res) => {
-//   res.render("results");
-// });
-// app.get("/test.ejs", (req, res) => {
-//   res.render("test");
-// });
-// app.get("/login.ejs", (req, res) => {
-//   res.render("login");
-// });
-// app.get("/signup.ejs", (req, res) => {
-//   res.render("signup");
-// });
-// app.get("/welcome.ejs", (req, res) => {
-//   res.render("welcome");
-// });
-// app.get("/home.ejs", (req, res) => {
-//   res.render("home");
-// });
-// app.get("/createTest.ejs", (req, res) => {
-//   res.render("createTest", { numQuestions: 0 }); // Ensure numQuestions is passed
-// });
-
-// app.post("/create-test-step1", (req, res) => {
-//   const numQuestions = req.body.numQuestions;
-//   res.render("createTest", { numQuestions: numQuestions });
-// });
-
-// app.post("/submit", (req, res) => postResFunc(req, res));
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
