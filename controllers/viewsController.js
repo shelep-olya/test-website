6bebdabfb779391ffc191a1c2ab7d35be057ea69
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-06-18 09:56:56 +0300

    finishing ejs establishing

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,27 +2,31 @@ const { postResFunc } = require("../utils/test-functionallity");
 exports.getHomePage = (req, res) => {
   res.render("main", {
     title: "home",
+    user: false,
   });
 };
 exports.getAboutPage = (req, res) => {
   res.render("about", {
     title: "about",
+    user: false,
   });
 };
 exports.getTestPage = (req, res) => {
   res.render("test", {
     title: "test",
+    user: false,
   });
 };
 exports.getResultsPage = (req, res) => {
   res.render("results", {
     title: "your results",
+    user: false,
   });
 };
 exports.logIn = (req, res) => {
-  res.render("login", { title: "log in" });
+  res.render("login", { title: "log in", user: false });
 };
 exports.signUp = (req, res) => {
-  res.render("signup", { title: "sign up" });
+  res.render("signup", { title: "sign up", user: false });
 };
 exports.submitBTN = (req, res) => postResFunc(req, res);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/me", (req, res) => {
   const locals = {
     title: "my account",
-    isAuth: true,
+    user: true,
   };
   res.render("me.ejs", { locals });
 });
@@ -12,29 +12,29 @@ router.get("/me", (req, res) => {
 router.get("/moreTests", (req, res) => {
   const locals = {
     title: "more tests",
-    isAuth: true,
+    user: true,
   };
   res.render("moreTests.ejs", { locals });
 });
 router.get("/welcome", (req, res) => {
   const locals = {
     title: "welcome",
-    isAuth: true,
+    user: true,
   };
   res.render("/welcome.ejs", { locals });
 });
 router.get("/home", (req, res) => {
   const locals = {
     title: "home",
-    isAuth: true,
+    user: true,
   };
-  res.render("home.ejs", { locals });
+  res.render("/home.ejs", { locals });
 });
 
 router.get("/createTest", (req, res) => {
   const locals = {
     title: "create test",
-    isAuth: true,
+    user: true,
   };
   res.render("createTest.ejs", { locals });
 });
