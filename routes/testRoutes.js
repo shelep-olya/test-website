0820e1312d178ad8e0099c9055340057a439727f
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-07-27 10:19:58 +0300

    added clickable test-links

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,8 +1,11 @@
 const testController = require("./../controllers/testController");
 const authController = require("./../controllers/authController");
+const viewsController = require("./../controllers/viewsController");
 const express = require("express");
 const router = express.Router();
 router.post("/addNumberOfQuestions", testController.addNumberOfQuestions);
 router.post("/submitTest", authController.protect, testController.submitTest);
 router.get("/myTests", authController.protect, testController.getMyTests);
+
+router.get("/test/:id", authController.protect, viewsController.getTestPage);
 module.exports = router;
